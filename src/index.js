33eaa2b916602a3e7e63732b61c81651d1a888dd
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-11-13 17:30:41 -0200

    set inti style ok

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import './styles/fonts.css';
+import './styles/typography.css';
+import './styles/base.css';
+
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
