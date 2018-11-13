ff75d14d8eda3da415b010bdba26f0a9fb1422dd
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-11-13 18:28:51 -0200

    changes style folder strcutrure to 7x1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Router, Route, browserHistory } from 'react-router';
+import './styles/abstracts/fonts.css';
+import './styles/base/base.css';
+import './styles/base/typography.css';
 
-import './styles/fonts.css';
-import './styles/typography.css';
-import './styles/base.css';
+import './styles/components/form.css';
 
 import App from './App';
-import SignupPage from './pages/SignupPage/SignupPage';
 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
 
 serviceWorker.unregister();
