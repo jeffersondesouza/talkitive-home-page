6e7869d30f775fc6d5dedc0b79283ee83e43e720
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-11-14 11:27:09 -0200

    build on repository

---
+++ build/precache-manifest.c7258a06e25f61f5eab7a7bebc6ce5b6.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c7409b8c5420ebd6022acae44779108",
+    "url": "/static/media/CircularStd-BookItalic.3c7409b8.woff"
+  },
+  {
+    "revision": "43cc276488e87531a843",
+    "url": "/static/css/main.16b63cd6.chunk.css"
+  },
+  {
+    "revision": "8ee08650291af620cc9c",
+    "url": "/static/js/1.8ee08650.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cff6bf5c918709ec72b81226fe63e6b5",
+    "url": "/static/media/talktive.cff6bf5c.png"
+  },
+  {
+    "revision": "829012af195c90c00bac2953653768a8",
+    "url": "/static/media/CircularStd-Book.829012af.woff"
+  },
+  {
+    "revision": "381f87a977e09572d69be5e7f6087fe4",
+    "url": "/static/media/CircularStd-Medium.381f87a9.woff"
+  },
+  {
+    "revision": "43cc276488e87531a843",
+    "url": "/static/js/main.43cc2764.chunk.js"
+  },
+  {
+    "revision": "451b6f03fc497603eebc23d24a97bbb0",
+    "url": "/static/media/CircularStd-MediumItalic.451b6f03.woff"
+  },
+  {
+    "revision": "03e7ee16210905484b09316e8298d712",
+    "url": "/static/media/CircularStd-Bold.03e7ee16.woff"
+  },
+  {
+    "revision": "705bbdf2db3c4668c8750804ca12d962",
+    "url": "/static/media/CircularStd-BoldItalic.705bbdf2.woff"
+  },
+  {
+    "revision": "49611f2982d0e8ef61372afe9850535b",
+    "url": "/static/media/CircularStd-Black.49611f29.woff"
+  },
+  {
+    "revision": "1c46c3018afa9b775e395b2076943ede",
+    "url": "/static/media/CircularStd-BlackItalic.1c46c301.woff"
+  },
+  {
+    "revision": "52964fd86238b508783d111e165fe797",
+    "url": "/index.html"
+  }
+];
